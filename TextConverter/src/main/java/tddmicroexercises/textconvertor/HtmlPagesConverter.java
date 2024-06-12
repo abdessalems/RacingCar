9f79e60a160200bd33de64eb2c44254bf87a5072
Baganda49 <abdessalemsaa@gmail.com>
2024-06-12 18:49:14 +0200

    Update HtmlPagesConverter for Testability and Add Test

--- TextConverter/src/main/java/tddmicroexercises/textconvertor/HtmlPagesConverter.java
+++ TextConverter/src/main/java/tddmicroexercises/textconvertor/HtmlPagesConverter.java
@@ -6,51 +6,52 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.text.StringEscapeUtils;
+
 public class HtmlPagesConverter {
 
     private String filename;
     private List<Integer> breaks = new ArrayList<Integer>();
-    
+
     public HtmlPagesConverter(String filename) throws IOException {
         this.filename = filename;
 
         this.breaks.add(0);
-        BufferedReader reader = new BufferedReader(new FileReader(this.filename));
-        int cumulativeCharCount = 0;
-        String line = reader.readLine();
-        while (line != null)
-        {
-            cumulativeCharCount += line.length() + 1; // add one for the newline
-            if (line.contains("PAGE_BREAK")) {
-                int page_break_position = cumulativeCharCount;
-                breaks.add(page_break_position);
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.filename))) {
+            int cumulativeCharCount = 0;
+            String line = reader.readLine();
+            while (line != null)
+            {
+                cumulativeCharCount += line.length() + 1; // add one for the newline
+                if (line.contains("PAGE_BREAK")) {
+                    int page_break_position = cumulativeCharCount;
+                    breaks.add(page_break_position);
+                }
+                line = reader.readLine();
             }
-            line = reader.readLine();
         }
-        reader.close();
     }
 
     public String getHtmlPage(int page) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(this.filename));
-        reader.skip(breaks.get(page));
-        StringBuffer htmlPage = new StringBuffer();
-        String line = reader.readLine();
-        while (line != null)
-        {
-            if (line.contains("PAGE_BREAK")) {
-                break;
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.filename))) {
+            reader.skip(breaks.get(page));
+            StringBuilder htmlPage = new StringBuilder();
+            String line = reader.readLine();
+            while (line != null)
+            {
+                if (line.contains("PAGE_BREAK")) {
+                    break;
+                }
+                htmlPage.append(StringEscapeUtils.escapeHtml(line));
+                htmlPage.append("<br />");
+
+                line = reader.readLine();
             }
-            htmlPage.append(StringEscapeUtils.escapeHtml(line));
-            htmlPage.append("<br />");
-            
-            line = reader.readLine();
+            return htmlPage.toString();
         }
-        reader.close();
-        return htmlPage.toString();
     }
 
     public String getFilename() {
         return this.filename;
     }
-    
 }
