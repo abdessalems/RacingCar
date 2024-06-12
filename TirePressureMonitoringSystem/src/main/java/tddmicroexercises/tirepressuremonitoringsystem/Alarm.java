13351ee2a0a87cc76facb263b0f4cb6f500d2feb
Baganda49 <abdessalemsaa@gmail.com>
2024-06-12 18:55:56 +0200

    Refactor Alarm Class to Improve Testability

--- TirePressureMonitoringSystem/src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Alarm.java
+++ TirePressureMonitoringSystem/src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Alarm.java
@@ -1,26 +1,26 @@
 package tddmicroexercises.tirepressuremonitoringsystem;
 
-public class Alarm
-{
+public class Alarm {
     private static final double LOW_PRESSURE_THRESHOLD = 17;
     private static final double HIGH_PRESSURE_THRESHOLD = 21;
 
-    protected Sensor sensor = new Sensor();
+    private Sensor sensor;
 
     protected boolean alarmOn = false;
 
-    public void check()
-    {
+    public Alarm(Sensor sensor) {
+        this.sensor = sensor;
+    }
+
+    public void check() {
         double psiPressureValue = sensor.popNextPressurePsiValue();
 
-        if (psiPressureValue < LOW_PRESSURE_THRESHOLD || HIGH_PRESSURE_THRESHOLD < psiPressureValue)
-        {
+        if (psiPressureValue < LOW_PRESSURE_THRESHOLD || HIGH_PRESSURE_THRESHOLD < psiPressureValue) {
             alarmOn = true;
         }
     }
 
-    public boolean isAlarmOn()
-    {
-        return alarmOn; 
+    public boolean isAlarmOn() {
+        return alarmOn;
     }
 }
